2b345b9f3be20041b2e7900ac931d08985e7b360
Flashfundbeta <119822902+Flashfundbeta@users.noreply.github.com>
2022-12-14 18:33:20 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-`use strict` var datetime = new Date(); console.log(datetime); document.getElementById("time").textContent =
+`use strict` var datetime = new Date(); console.log(datetime); 
+document.getElementById("time").textContent =
 `use strict` 
 var datetime = new Date(); console.log(datetime);
 document.getElementById("time").textContent =
